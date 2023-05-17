149c5bbc9ea4f75a153ef53119c7378051000002
Wael Elsafty <waelelsafty07@gmail.com>
2023-05-17 15:02:43 +0300

    Add functionalty to submit score

---
+++ src/js/submitScore.js
@@ -0,0 +1,21 @@
+import LeaderBoard from './leaderBoard.js';
+
+const leaderboard = new LeaderBoard();
+const submitBtn = document.querySelector('.btn-submit');
+
+if (submitBtn) {
+  const nameInput = document.querySelector('.name');
+  const scoreInput = document.querySelector('.score');
+  submitBtn.addEventListener('click', async (e) => {
+    e.preventDefault();
+    const user = nameInput.value;
+    const score = scoreInput.value;
+    const body = {
+      user,
+      score,
+    };
+    if (user && score) {
+      await leaderboard.addNewScores(body);
+    }
+  });
+}
